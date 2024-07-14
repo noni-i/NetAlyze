d1d58dd8c96d68dae51aebd4267dc6e3cbf3fb4d
noni-i <120342606+noni-i@users.noreply.github.com>
2024-07-14 20:18:20 +0200

    14.07.2024

---
+++ venv/netalyze.py
@@ -0,0 +1,109 @@
+from scapy.all import sniff, IP, TCP, UDP, ICMP
+import re
+import datetime
+
+def spaces(length):
+    totalL = len(length)
+    spaces = ' ' * (19 - totalL)
+    return spaces
+
+def spacesPort(length):
+    port = str(length)
+    totalL = len(port)
+    spaces = ' ' * (9 - totalL)
+    return spaces
+
+def packet_callback(packet):
+
+    packet_time = datetime.datetime.fromtimestamp(packet.time)
+    time = packet_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    if packet.haslayer(TCP):
+        if packet.haslayer(IP):
+            ip_layer = packet.getlayer(IP)
+            tcp_layer = packet.getlayer(TCP)
+            print(f"| {time}    | {ip_layer.src}{spaces(ip_layer.src)}| {tcp_layer.sport}{spacesPort(tcp_layer.sport)}| -> | {ip_layer.dst}{spaces(ip_layer.dst)}| {tcp_layer.dport}{spacesPort(tcp_layer.dport)}| TCP                |")
+
+    if packet.haslayer(UDP):
+        if packet.haslayer(IP):
+            ip_layer = packet.getlayer(IP)
+            udp_layer = packet.getlayer(UDP)
+            print(f"| {time}    | {ip_layer.src}{spaces(ip_layer.src)}| {udp_layer.sport}{spacesPort(udp_layer.sport)}| -> | {ip_layer.dst}{spaces(ip_layer.dst)}| {udp_layer.dport}{spacesPort(udp_layer.dport)}| UDP                |")
+
+    if packet.haslayer(ICMP):
+        if packet.haslayer(IP):
+            ip_layer = packet.getlayer(IP)
+            icmp_layer = packet.getlayer(ICMP)
+            print(f"| {time}    | {ip_layer.src}{spaces(ip_layer.src)}|          | -> | {ip_layer.dst}{spaces(ip_layer.dst)}|          | ICMP Type {icmp_layer.type} Code {icmp_layer.code} |")
+
+def get_filter_input(filter):
+
+    filterRe = re.search("filter\([^)]*\)", filter)
+    filter_parts = filterRe.group().lower().split(",")
+
+    filter_complete = []
+
+    if filter_parts:
+        for i in filter_parts:
+            address = re.search(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', i)
+            if address:
+                if f"ip.addr=\"{address.group(0)}\"" in i:
+                    filter_complete.append(f"host {address.group(0)}")
+
+            if "tcp" in i:
+                filter_complete.append("tcp")
+
+            if "udp" in i:
+                filter_complete.append("udp")
+
+            if "icmp" in i:
+                filter_complete.append("icmp")
+
+            port = re.search(r'([0-9]+)', i)
+            if port:
+                if f"port=\"{port.group(0)}\"" in i:
+                    filter_complete.append(f"port {port.group(0)}")
+
+    return " and ".join(filter_complete)
+
+
+if __name__ == "__main__":
+
+    BLUE = "\033[34m"
+    RESET = "\033[0m"
+
+    print(f"{BLUE}      ___           ___                       ___                                     ___           ___    ")
+    print(f"{BLUE}     /__/\         /  /\          ___        /  /\                        ___        /  /\         /  /\   ")
+    print(f"{BLUE}     \  \:\       /  /:/_        /  /\      /  /::\                      /__/|      /  /::|       /  /:/_  ")
+    print(f"{BLUE}      \  \:\     /  /:/ /\      /  /:/     /  /:/\:\    ___     ___     |  |:|     /  /:/:|      /  /:/ /\ ")
+    print(f"{BLUE}  _____\__\:\   /  /:/ /:/_    /  /:/     /  /:/~/::\  /__/\   /  /\    |  |:|    /  /:/|:|__   /  /:/ /:/_")
+    print(f"{BLUE} /__/::::::::\ /__/:/ /:/ /\  /  /::\    /__/:/ /:/\:\ \  \:\ /  /:/  __|__|:|   /__/:/ |:| /\ /__/:/ /:/ /\ ")
+    print(f"{BLUE} \  \:\~~\~~\/ \  \:\/:/ /:/ /__/:/\:\   \  \:\/:/__\/  \  \:\  /:/  /__/::::\   \__\/  |:|/:/ \  \:\/:/ /:/")
+    print(f"{BLUE}  \  \:\  ~~~   \  \::/ /:/  \__\/  \:\   \  \::/        \  \:\/:/      ~\~~\:\      |  |:/:/   \  \::/ /:/")
+    print(f"{BLUE}   \  \:\        \  \:\/:/        \  \:\   \  \:\         \  \::/         \  \:\     |  |::/     \  \:\/:/ ")
+    print(f"{BLUE}    \  \:\        \  \::/          \__\/    \  \:\         \__\/           \__\/     |  |:/       \  \::/  ")
+    print(f"{BLUE}     \__\/         \__\/                     \__\/                                   |__|/         \__\/   ")
+    print(" ")
+    print(" ")
+    print(f"{BLUE}					            .:..:. Coded by @erjonahmeti .:..:.{RESET}")
+    print(" ")
+    print("     Netalyze is a packet sniffer coded in Python, designed for network monitoring and analysis.")
+    print("This tool is capable of capturing and filtering network packets based on various criteria, including IP")
+    print("                 addresses, protocols (TCP, UDP, ICMP), and port numbers.")
+    print(" ")
+    print("The filter format is as it goes \"filter(argument)\" ")
+    print("The arguments can be ip.addr=\"ipv4 here\", tcp, udp, icmp, or port=\"port here\"")
+    print("e.g. filter(ip.addr=\"192.168.0.1\", tcp, port=\"80\")")
+    print(" ")
+    input = input("If you have a filter enter here using the format above (leave blank if not): ")
+
+    if input:
+        filter_string = get_filter_input(input)
+        print(f"Using filter: {filter_string}")
+        print("| Time                   | Source             | SPort    |    | Destination        | Dport    | Protocol           |")
+        print("--------------------------------------------------------------------------------------------------------------------")
+        sniff(filter=filter_string, prn=packet_callback, store=0)
+    else:
+        print("| Time                   | Source             | SPort    |    | Destination        | Dport    | Protocol           |")
+        print("--------------------------------------------------------------------------------------------------------------------")
+        sniff(prn=packet_callback, store=0)
