c396b5135b4bff1365c7277ada6c7d1976730413
noni-i <120342606+noni-i@users.noreply.github.com>
2024-07-15 23:19:10 +0200

    15.07.2024

--- netalyze.py
+++ netalyze.py
@@ -1,41 +1,48 @@
+#scapy to capture the packets
 from scapy.all import sniff, IP, TCP, UDP, ICMP
 import re
 import datetime
 
+#generates the spaces after the ips during execution
 def spaces(length):
     totalL = len(length)
     spaces = ' ' * (19 - totalL)
     return spaces
-
+#generates the spaces after the ports during execution
 def spacesPort(length):
     port = str(length)
     totalL = len(port)
     spaces = ' ' * (9 - totalL)
     return spaces
 
+#the function of packet capturing
 def packet_callback(packet):
 
     packet_time = datetime.datetime.fromtimestamp(packet.time)
     time = packet_time.strftime('%Y-%m-%d %H:%M:%S')
 
+    #prints out TCP packets
     if packet.haslayer(TCP):
         if packet.haslayer(IP):
             ip_layer = packet.getlayer(IP)
             tcp_layer = packet.getlayer(TCP)
             print(f"| {time}    | {ip_layer.src}{spaces(ip_layer.src)}| {tcp_layer.sport}{spacesPort(tcp_layer.sport)}| -> | {ip_layer.dst}{spaces(ip_layer.dst)}| {tcp_layer.dport}{spacesPort(tcp_layer.dport)}| TCP                |")
 
+    # prints out UDP packets
     if packet.haslayer(UDP):
         if packet.haslayer(IP):
             ip_layer = packet.getlayer(IP)
             udp_layer = packet.getlayer(UDP)
             print(f"| {time}    | {ip_layer.src}{spaces(ip_layer.src)}| {udp_layer.sport}{spacesPort(udp_layer.sport)}| -> | {ip_layer.dst}{spaces(ip_layer.dst)}| {udp_layer.dport}{spacesPort(udp_layer.dport)}| UDP                |")
 
+    # prints out ICMP packets
     if packet.haslayer(ICMP):
         if packet.haslayer(IP):
             ip_layer = packet.getlayer(IP)
             icmp_layer = packet.getlayer(ICMP)
             print(f"| {time}    | {ip_layer.src}{spaces(ip_layer.src)}|          | -> | {ip_layer.dst}{spaces(ip_layer.dst)}|          | ICMP Type {icmp_layer.type} Code {icmp_layer.code} |")
 
+#filter function
 def get_filter_input(filter):
 
     filterRe = re.search("filter\([^)]*\)", filter)
@@ -69,6 +76,7 @@ def get_filter_input(filter):
 
 if __name__ == "__main__":
 
+    #color
     BLUE = "\033[34m"
     RESET = "\033[0m"
 
@@ -97,6 +105,7 @@ if __name__ == "__main__":
     print(" ")
     input = input("If you have a filter enter here using the format above (leave blank if not): ")
 
+    #conditions for filtering
     if input:
         filter_string = get_filter_input(input)
         print(f"Using filter: {filter_string}")
